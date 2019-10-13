509edad1603c5cd6b2501136a673f85bff3c8474
David Gillette <55262929+AmpedUpCodeLark@users.noreply.github.com>
2019-10-13 10:24:16 -0500

    Odds.py No Detail

---
+++ odds.py
@@ -0,0 +1,24 @@
+# Head First Python - odds code
+# David Gillette
+# 630-975-7282
+# jupyter editor
+
+from datetime import datetime
+
+import random
+import time
+
+odds = [1, 3, 5, 7, 9, 11, 13, 15 ,17, 19,
+        21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+        41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
+
+for i in range(5):
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds:
+        print("The odds might be with us!")
+    else:
+        print("On second thought; the odds aren't with us!")
+    wait_time = random.randint(1, 60)
+    time.sleep(wait_time)
+    
+    
