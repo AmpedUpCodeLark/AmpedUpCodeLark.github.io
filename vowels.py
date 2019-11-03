09557461cfb171ad02163167836fc77c5c242008
David Gillette <55262929+AmpedUpCodeLark@users.noreply.github.com>
2019-11-03 16:01:38 -0600

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,10 @@
+# Head First Python - vowels code
+# David Gillette
+# 630-975-7282
+# jupyter editor
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = "Milliways"
+for letter in word:
+        if letter in vowels:
+                print(letter)
