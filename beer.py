e38d76383e2f1df22f5538a7a58bbae07985b3f7
David Gillette <55262929+AmpedUpCodeLark@users.noreply.github.com>
2019-11-03 15:38:27 -0600

    Create beer.py

---
+++ beer.py
@@ -0,0 +1,22 @@
+# Head First Python - beer code
+# David Gillette
+# 630-975-7282
+# jupyter editor
+
+import time
+
+word = "bottles"
+for beer_num in range(99, 0, -1):
+    print(beer_num, word, "of beer on the wall.")
+    print(beer_num, word, "of beer.")
+    print("Take one down.")
+    print("Pass it around.")
+    if beer_num == 1:
+        print("No more bottles of beer on the wall.")
+    else:
+        new_num = beer_num - 1
+        if new_num == 1:
+            word = "bottle"
+        print(new_num, word, "of beer on the wall.")
+        time.sleep(1)
+    print()
