9896489dc55a90de0312150f5ce6ed6aefe404e1
David Gillette <55262929+AmpedUpCodeLark@users.noreply.github.com>
2019-11-07 10:06:08 -0600

    Create panic.py

---
+++ panic.py
@@ -0,0 +1,20 @@
+# Head First Python - panic
+# David Gillette
+# 630-975-7282
+# jupyter editor
+
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+
+for i in range(4):
+    plist.pop()
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
+
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
