445e39ee3f9926563b8044623165f1427a59037c
David Gillette <55262929+AmpedUpCodeLark@users.noreply.github.com>
2019-11-16 07:18:40 -0600

    Create marvin.py

---
+++ marvin.py
@@ -0,0 +1,9 @@
+# Head First Python - marvin
+# David Gillette
+# 630-975-7282
+# jupyter editor
+
+paranoid_android = "Marvin"
+letters = list(paranoid_android)
+for char in letters:
+        print('\t', char)
