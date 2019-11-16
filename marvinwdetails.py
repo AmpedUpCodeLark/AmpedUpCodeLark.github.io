f20797109f892f83c922bdcff88e301e5411ea9e
David Gillette <55262929+AmpedUpCodeLark@users.noreply.github.com>
2019-11-16 07:19:18 -0600

    Create marvinwdetails.py

---
+++ marvinwdetails.py
@@ -0,0 +1,19 @@
+# Head First Python - marvin with details
+# David Gillette
+# 630-975-7282
+# jupyter editor
+
+
+# the string "Marvin is assigned to the variable paranoid_android
+paranoid_android = "Marvin"
+
+# the variable letters is assigned the list created from the
+# string paranoid_android
+letters = list(paranoid_android)
+
+# each iternation of letters is assigned to the variable char
+for char in letters:
+    
+# each iternation of the char varaible is print preceeded by a tab
+# '\t' creates a tab
+        print('\t', char)
